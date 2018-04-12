ee6a8cac820570c008580a045c25bd47bf1fe81b
Gita Guntaka <gitasri.reddy@gmail.com>
2018-04-11 17:13:41 -0700

    Javacoco repor plugin added. Added unit test cases.

--- src/main/java/com/gita/ds/App.java
+++ src/main/java/com/gita/ds/App.java
@@ -1,5 +1,8 @@
 package com.gita.ds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        List<Integer>  temp = new ArrayList<Integer>();
+        temp.add(1);
+        
+    }
+    
+    public int add(int x, int y) {
+    	return x + y;
+    }
+    
+    public boolean isEven(int x) {
+    	if (x % 2 == 0) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 }
--- src/test/java/com/gita/ds/AppTest.java
+++ src/test/java/com/gita/ds/AppTest.java
@@ -1,38 +1,22 @@
 package com.gita.ds;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.testng.annotations.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  @Test
+  public void testAdd() {
+	  App app = new App();
+	  assertEquals(app.add(5, 6), 11, "faile add");
+  }
+  
+  @Test
+  public void testIsEven() {
+	  App app = new App();
+	  assertTrue(app.isEven(4));
+	  
+  }
 }
